2a762d90245c044ed43309458c04ae4104a4ba97
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-03-30 02:41:01 -0300

    Add files via upload

---
+++ lista 0_1/JS/atividade1.js
@@ -0,0 +1,19 @@
+var pessoa1 = "Miguel"  
+var idade1 = 22
+var pessoa2 = "carlos"
+var idade2 = 13
+var pessoa3 = "ana"
+var idade3 = 49
+var pessoa4 = "josé"
+var idade4 = 37
+var pessoa5 ="marcia"
+var idade = 71
+console.log(pessoa1 + " - " + idade1 + " anos")
+console.log(pessoa2 + " - " + idade2 + " anos")
+console.log(pessoa3 + " - " + idade3 + " anos")
+console.log(pessoa4 + " - " + idade4 + " anos")
+console.log(pessoa5 + " - " + idade5 + " anos" ) 
+
+
+
+
---
+++ lista 0_1/JS/aula.md
@@ -0,0 +1,9 @@
+# extensões de arquivos
+    .por
+    .html
+    .js
+    .css
+    .cs
+    .java
+    .mp4
+    .mp3
