605708e3ecb54437064bd2b91ab54fe8f4cc6419
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-04-27 18:32:42 -0300

    Update Main.java

--- lista 5 p/src/Main.java
+++ lista 5 p/src/Main.java
@@ -1,6 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
 
-        Heroi heroi = new Heroi(1,"isaac");
+
+       public static void main(String[] args) {
+        Grupo grupo = new Grupo();
+
+        grupo.adicionarHeroi(new Heroi(1, "homen Aranha", 70));
+        grupo.adicionarHeroi(new Heroi(2, "HULK", 120));
+        grupo.adicionarHeroi(new Heroi(3, "groot", 150));
+        grupo.adicionarHeroi(new Heroi(4, "thor", 150));
+
+        grupo.removerHeroi(1);
+
+        System.out.println("O total de moedas do grupo é: " + grupo.getTotalMoedasDeOuro());
+
     }
-}
