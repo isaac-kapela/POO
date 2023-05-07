c322e91f7586db8e94f1563da4347cc79663e6d4
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-05-07 00:52:23 -0300

    Add files via upload

---
+++ Lista 6/EX_2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado();
+        System.out.println( " A area do quadrado é: " + quadrado.calcularArea(15,3));
+
+        Triangulo triangulo = new Triangulo();
+        System.out.println(" A area do quadrado é: " + triangulo.calcularArea(18,5));
+    }
+}
