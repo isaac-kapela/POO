4db1b4377dd93de151bb6870ad64e343be2c27b2
ice <ice@lab06m33.nrc.ice.ufjf.br>
2025-06-24 18:00:59 -0300

    aa

--- teste01/src/main/java/org/example/Compra.java
+++ teste01/src/main/java/org/example/Compra.java
@@ -4,13 +4,13 @@ public class Compra extends  Transacao{
     private int precoUnidade;
     private Fornecedor fornecedor;
 
-
-    public Compra(String dataTransacao, int quant, Produto produto, int precoUnidade, Fornecedor fornecedor) {
+    public Compra(String dataTransacao, Fornecedor fornecedor, int quant, Produto produto) {
         super(dataTransacao, quant, produto);
     }
 
-    public void comprar(){
-        
+
+    public void comprar(String dataTrasancao, Fornecedor fornecedor, int quantidadeCompra, int precoUnidade){
+
     }
 
 
